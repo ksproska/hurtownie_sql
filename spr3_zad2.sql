17d82148f3cdb6950f675426c19590003fd82cee
Kamila Sproska <61067969+ksproska@users.noreply.github.com>
2022-03-26 10:57:27 +0100

    zad 2 finished

---
+++ spr3_zad2.sql
@@ -0,0 +1,22 @@
+WITH names AS (
+	SELECT 
+		CONCAT(p.LastName, ', ', p.FirstName) AS [Pracownik], 
+		e.BusinessEntityID AS [pracID]
+	FROM 
+		[HumanResources].[Employee] e
+	FULL OUTER JOIN [Person].[Person] p
+	ON e.BusinessEntityID = p.BusinessEntityID
+)
+SELECT DISTINCT
+	names.Pracownik,
+	[SalesPersonID] AS [pracID],
+	YEAR([OrderDate]) AS [Rok zamównienia], 
+	SUM([SubTotal]) OVER(PARTITION BY [SalesPersonID], YEAR([OrderDate])) AS [Kwota],
+	COUNT([SalesOrderID]) OVER(PARTITION BY [SalesPersonID], YEAR([OrderDate])) AS [Liczba zamówieñ]
+FROM 
+	[Sales].[SalesOrderHeader] sh
+INNER JOIN names
+ON names.pracID = sh.SalesPersonID
+WHERE [SalesPersonID] IS NOT NULL
+ORDER BY [pracID], YEAR([OrderDate])
+;
